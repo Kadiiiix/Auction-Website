d85875e72c984c087abcadbb0b9cc74bc74ed5bb
Amina Kadic <kadicamina01@gmail.com>
2024-05-09 16:49:27 +0200

    favorites v2

--- backend/controllers/favoriteControllers.js
+++ backend/controllers/favoriteControllers.js
@@ -1,96 +1,61 @@
 const User = require("../models/userModel");
-const bcrypt = require("bcrypt");
-
-exports.registerUser = async (req, res) => {
+const Auction = require("../models/auctionModel");
+exports.addToFavorites = async (req, res) => {
+  const auctionId = req.params.auctionId;
+  const userId = req.params.userId;
   try {
-    const { email, username, password } = req.body;
+    const auction = await Auction.findById(auctionId);
+    const user = await User.findById(userId);
 
-    // Check if the email is already registered
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-      return res.status(400).json({ error: "Email already registered." });
-    }
-
-    // Check if the username is already taken
-    const existingUsername = await User.findOne({ username });
-    if (existingUsername) {
-      return res.status(400).json({ error: "Username already taken." });
-    }
-
-    // Password validation
-    const passwordIsValid = validatePassword(password);
-    if (!passwordIsValid) {
-      return res
-        .status(400)
-        .json({
-          error:
-            "Invalid password. Password must be at least 8 characters long and contain at least one uppercase letter, one number, and one special character.",
-        });
-    }
-
-    // Password hashing
-    const hashedPassword = await hashPassword(password);
-
-    // Create a new user with hashed password
-    const newUser = new User({ email, username, password: hashedPassword });
-    await newUser.save();
-
-    return res.status(200).json({ message: "User registered successfully." });
-  } catch (error) {
-    console.error("Error registering user:", error);
-    return res.status(500).json({ error: "Internal server error." });
-  }
-};
-
-exports.loginUser = async (req, res) => {
-  try {
-    const { email, password } = req.body;
-
-    // Find the user by email
-    const user = await User.findOne({ email });
-
-    // If user doesn't exist, return error
-    if (!user) {
-      return res.status(404).json({ error: "User not found." });
-    }
-
-    // Compare passwords
-    const passwordMatch = await bcrypt.compare(password, user.password);
-    if (!passwordMatch) {
-      return res.status(401).json({ error: "Incorrect password." });
-    }
-
-    // If user exists and password is correct, return success message
-    return res.status(200).json({ message: "Login successful.", user });
-  } catch (error) {
-    console.error("Error logging in:", error);
-    return res.status(500).json({ error: "Internal server error." });
-  }
-};
-
-// Password validation function
-function validatePassword(password) {
-  const passwordRegex =
-    /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d!@#$%^&*()_+]{8,}$/;
-  return passwordRegex.test(password);
-}
-
-// Password hashing function
-async function hashPassword(password) {
-  const saltRounds = 10;
-  return await bcrypt.hash(password, saltRounds);
-}
-
-exports.listFavorites = async (req, res) => {
-  try {
-    const userId = req.params.userId;
-    const user = await User.findById(userId).populate("favorites");
     if (!user) {
       return res.status(404).json({ error: "User not found" });
     }
-    res.status(200).json(user.favorites);
+    if (!auction) {
+      return res.status(404).json({ error: "Auction not found" });
+    }
+
+    if (user.favorites.includes(auctionId)) {
+      return res.status(400).json({ error: "Auction already in favorites" });
+    }
+
+    // Add the auction ID to the user's favorites array
+    user.favorites.push(auctionId);
+    auction.likedBy.push(userId);
+    await user.save();
+    await auction.save();
+
+    res
+      .status(200)
+      .json({ message: "Auction added to favorites successfully", user });
   } catch (error) {
-    console.error("Error listing favorites:", error);
+    console.error("Error adding auction to favorites:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+exports.removeFromFavorites = async (req, res) => {
+  const auctionId = req.params.auctionId;
+  const userId = req.params.userId;
+
+  try {
+    // Find the user by ID
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    // Remove the auction ID from the user's favorites array
+    user.favorites = user.favorites.filter(
+      (fav) => fav.toString() !== auctionId
+    );
+    await user.save();
+
+    res
+      .status(200)
+      .json({ message: "Auction removed from favorites successfully", user });
+  } catch (error) {
+    console.error("Error removing auction from favorites:", error);
     res.status(500).json({ error: "Internal server error" });
   }
 };
