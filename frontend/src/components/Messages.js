45e720b9ad5f39b29494506265f196c0ada8d0f2
Amina Kadic <kadicamina01@gmail.com>
2024-06-05 20:32:16 +0200

    mesages frontend v1

--- frontend/src/components/Messages.js
+++ frontend/src/components/Messages.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 import React, { useState, useEffect, useRef } from 'react';
 import { useParams, Link } from 'react-router-dom';
-import { Input, Button } from 'antd';
-import './../design/Message.css'; // Import custom CSS for styling
-
+import { Input, Button ,Avatar} from 'antd';
+import { UserOutlined} from "@ant-design/icons";
+import { format } from "date-fns";
+import './../design/Message.css'; 
 const { TextArea } = Input;
 
 function Messages() {
@@ -23,8 +24,15 @@ function Messages() {
         receiver: id,
         message: newMessage,
       });
-      console.log("Message sent", response.data);
-      setMessages([...messages, { sender: userId, message: newMessage, timestamp: new Date() }]);
+      
+       setMessages([
+         ...messages,
+         {
+           sender: { _id: userId, photo: sender.photo },
+           message: newMessage,
+           timestamp: new Date(),
+         },
+       ]);
       setNewMessage('');
       scrollToBottom();
       getMessages();
@@ -81,25 +89,55 @@ function Messages() {
   const scrollToBottom = () => {
     messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
   };
+function formatTimestamp(timestamp) {
+  const date = new Date(timestamp);
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  const month = months[date.getMonth()];
+  const day = date.getDate();
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+
+  return `${month} ${day},  ${hours}:${minutes}`;
+}
 
 
   return (
     <div className="messages-container">
-      {id!=="user" && oldMessages.length === 0 && (
+      {id !== "user" && oldMessages.length === 0 && (
         <h2>Start conversation with {receiver} by sending a message.</h2>
       )}
-      {id!=="user" && oldMessages.length > 0 && (
-        <h2>Chat with {receiver}</h2>
-      )}
-      {id!=="user" && oldMessages.length > 0 && (
+      {id !== "user" && oldMessages.length > 0 && <h2>Chat with {receiver}</h2>}
+      {id !== "user" && oldMessages.length > 0 && (
         <div className="messages-list">
           {oldMessages.map((message, index) => (
             <div className="message-rectangle" key={index}>
-              <div className="message-info">
-                <Link to={`/profile/${message.sender}`}>
-                  {message.sender === userId ? `${sender} (You)` : receiver}
-                </Link>
-                <p>{new Date(message.timestamp).toLocaleString()}</p>
+              <div className="message-header">
+                <Avatar
+                  className="ph"
+                  shape="circle"
+                  size={60}
+                  src={message.sender.photo}
+                  icon={<UserOutlined />}
+                />
+                <div className="message-info">
+                  <Link to={`/profile/${message.sender}`}>
+                    {message.sender === userId ? `${sender} (You)` : receiver}
+                  </Link>
+                  <p className="time">{formatTimestamp(message.timestamp)}</p>
+                </div>
               </div>
               <div className="message-text">
                 <p>{message.message}</p>
@@ -109,7 +147,7 @@ function Messages() {
           <div ref={messagesEndRef} />
         </div>
       )}
-      {id!=="user" && (
+      {id !== "user" && (
         <div className="message-input-container">
           <TextArea
             placeholder="Type your message"
@@ -118,12 +156,12 @@ function Messages() {
             onChange={(e) => setNewMessage(e.target.value)}
             className="message-input"
           />
-          <Button type="primary" onClick={sendMessage} className="button">Send</Button>
+          <Button type="primary" onClick={sendMessage} className="button">
+            Send
+          </Button>
         </div>
       )}
-      {id==="user" && (
-        <h2>Select conversation or start a new one.</h2>
-      )}
+      {id === "user" && <h2>Select conversation or start a new one.</h2>}
     </div>
   );
 }  
--- frontend/src/components/MessagesSider.js
+++ frontend/src/components/MessagesSider.js
@@ -1,9 +1,9 @@
-import axios from 'axios';
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { Card } from 'antd';
-import './../design/MessageSider.css'; // Import custom CSS for styling
-import SearchUser from './SearchUser';
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Card } from "antd";
+import "./../design/MessageSider.css"; // Import custom CSS for styling
+import SearchUser from "./SearchUser";
 
 function MessagesSider() {
   const [conversations, setConversations] = useState([]);
@@ -12,7 +12,9 @@ function MessagesSider() {
   // Function to fetch user details based on user ID
   const fetchUser = async (user) => {
     try {
-      const response = await axios.get(`http://localhost:4000/api/users/${user}`);
+      const response = await axios.get(
+        `http://localhost:4000/api/users/${user}`
+      );
       return response.data; // Return the entire user data from the response
     } catch (error) {
       console.error("Error fetching user:", error);
@@ -23,21 +25,27 @@ function MessagesSider() {
   useEffect(() => {
     async function fetchConversations() {
       try {
-        const response = await axios.get('http://localhost:4000/api/messages/messages');
+        const response = await axios.get(
+          "http://localhost:4000/api/messages/messages"
+        );
         const messages = response.data;
 
         // Create an object to store last messages between users
         const lastMessages = {};
 
         // Iterate over messages to find the last message between each pair of users involving the signed-in user
-        messages.forEach(message => {
+        messages.forEach((message) => {
           const sender = message.sender;
           const receiver = message.receiver;
-          if (sender === userId || receiver === userId) { // Check if the message involves the signed-in user
-            const conversationKey = [sender, receiver].sort().join('-'); // Sort user IDs to create a unique conversation key
+          if (sender === userId || receiver === userId) {
+            // Check if the message involves the signed-in user
+            const conversationKey = [sender, receiver].sort().join("-"); // Sort user IDs to create a unique conversation key
 
             // Update last message if it's null or newer than the current message
-            if (!lastMessages[conversationKey] || message.timestamp > lastMessages[conversationKey].timestamp) {
+            if (
+              !lastMessages[conversationKey] ||
+              message.timestamp > lastMessages[conversationKey].timestamp
+            ) {
               lastMessages[conversationKey] = message;
             }
           }
@@ -47,28 +55,35 @@ function MessagesSider() {
         const conversationKeys = Object.keys(lastMessages);
 
         // Extract user details and last messages for each conversation involving the signed-in user
-        const conversations = await Promise.all(conversationKeys.map(async key => {
-          const [senderId, receiverId] = key.split('-');
-          const otherUserId = senderId === userId ? receiverId : senderId; // Get the ID of the other user
-          const userData = await fetchUser(otherUserId);
-          return {
-            userId: otherUserId,
-            username: userData.username,
-            lastMessage: lastMessages[key]
-          };
-        }));
+        const conversations = await Promise.all(
+          conversationKeys.map(async (key) => {
+            const [senderId, receiverId] = key.split("-");
+            const otherUserId = senderId === userId ? receiverId : senderId; // Get the ID of the other user
+            const userData = await fetchUser(otherUserId);
+            if (userData) {
+              return {
+                userId: otherUserId,
+                username: userData.username,
+                lastMessage: lastMessages[key],
+              };
+            } else {
+              return null;
+            }
+          })
+        );
 
-        // Update the state with conversations and user details
-        setConversations(conversations);
+        // Filter out any null conversations (in case user fetching failed)
+        setConversations(
+          conversations.filter((conversation) => conversation !== null)
+        );
       } catch (error) {
-        console.error('Error fetching conversations:', error);
+        console.error("Error fetching conversations:", error);
       }
     }
 
     fetchConversations();
-  }); // Include userId in the dependency array to fetch conversations when it changes
+  }, [userId]); // Include userId in the dependency array to fetch conversations when it changes
 
-  
   const handleUserSelect = (user) => {
     // Redirect to the conversation with the selected user
     window.location.href = `/messages/${user._id}`;
@@ -76,19 +91,24 @@ function MessagesSider() {
 
   return (
     <div className="sider-container">
-        <Card className="search"
-            style={{
-            width: 300,}}>
-            <p>Find user to message them:</p>
-            <SearchUser onUserSelect={handleUserSelect} />
-        </Card>
-      <h1>Conversations</h1>
+      <Card
+        className="search"
+        style={{
+          width: 300,
+        }}
+      >
+        <p>Find user to message them:</p>
+        <SearchUser onUserSelect={handleUserSelect} />
+      </Card>
+      <h1 className="convo">Conversations</h1>
       <div className="conversations-list">
         {conversations.map((conversation, index) => (
-          <Card key={index} style={{ width: '100%', marginBottom: '10px' }}>
+          <Card key={index} style={{ width: "100%", marginBottom: "10px" }}>
             <div className="conversation-info">
               <p className="username">{conversation.username}</p>
-              <p className="message-date">{new Date(conversation.lastMessage.timestamp).toLocaleString()}</p>
+              <p className="message-date">
+                {new Date(conversation.lastMessage.timestamp).toLocaleString()}
+              </p>
             </div>
             <div className="last-message">
               <p>Last message: {conversation.lastMessage.message}</p>
